04b94bab62704bb23d330ddf8b0b2665f4ca6609
goringich <actingsv@gmail.com>
2023-10-24 21:50:52 +0300

    ещё стили подправил

--- slick.js
+++ slick.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   $('.slider-for').slick({
+    centerPadding: '60px',
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
@@ -7,11 +8,32 @@ $(document).ready(function(){
     asNavFor: '.slider-nav'
   });
   $('.slider-nav').slick({
+    centerMode: true,
     slidesToShow: 9,
     slidesToScroll: 1,
     asNavFor: '.slider-for',
     dots: false,
-    centerMode: false,
-    focusOnSelect: false
+    arrows: false,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 });
