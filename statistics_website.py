6ae3477eb4216d85476027a2f4aa4b9cfcd9122c
rexizer <regerig@mail.ru>
2023-10-25 18:19:23 +0300

    added Flask and example of how profiles work

---
+++ statistics_website.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, g, url_for
+import sqlite3
+import json
+
+
+app = Flask(__name__)
+
+DATABASE = 'finance.db'
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def check_table_exists(sql, table_name):
+    sql.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}';")
+    return sql.fetchone()
+
+
+def get_spending(sql, table_name):
+    sql.row_factory = sqlite3.Row
+    rows = sql.execute(f"SELECT * from {table_name}").fetchall()
+    return json.dumps([dict(ix) for ix in rows], ensure_ascii=False)
+
+
+@app.route('/profile/<int:user_id>')
+def profile(user_id):
+    user_table = 'spending_' + str(user_id)
+
+    sql = get_db().cursor()
+    if check_table_exists(sql, user_table):
+        user_info = {
+            'id': user_id,
+            'email': 'johndoe@example.com',
+            'age': 30,
+            'city': 'Москва',
+            'about': get_spending(sql, user_table),
+            'profile_picture': 'https://example.com/profile-picture.jpg'
+        }
+
+        sql.close()
+        return render_template('profile.html', user_info=user_info)
+    else:
+        return page_not_found(404)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
